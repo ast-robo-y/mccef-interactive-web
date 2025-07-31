4862f466d9d5bdb13fcae68850e32d16ddff7491
ast-robo-y <robertjurenka@gmail.com>
2025-07-31 20:16:38 +0200

    Data Update 31.7.2025

--- public/home.py
+++ public/home.py
@@ -263,9 +263,9 @@ with coltext[0]:
                     )
 
     if not st.session_state['language']:
-        st.markdown('<span style="font-size:9pt; color: grey;">Poslední aktualizace: 22. července 2025</span>', unsafe_allow_html=True)
+        st.markdown('<span style="font-size:9pt; color: grey;">Poslední aktualizace: 31. července 2025</span>', unsafe_allow_html=True)
     else:
-        st.markdown('<span style="font-size:9pt; color: grey;">Last Update: 07/22/2025</span>', unsafe_allow_html=True)
+        st.markdown('<span style="font-size:9pt; color: grey;">Last Update: 07/31/2025</span>', unsafe_allow_html=True)
     with st.expander(label=("💬 Vysvětlení zkratek fondů" if not language_on else "💬 Explanation of Fund abbreviations")):
         st.markdown(funds_text)
     Partners()
